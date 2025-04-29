340c8c8323ed45520dd1459daeb5f59028b59781
Hyuse <165687332+hyus3@users.noreply.github.com>
2025-04-29 17:11:42 +0800

    finally react

--- ccs8_2/src/components1/faqs/Body3.tsx
+++ ccs8_2/src/components1/faqs/Body3.tsx
@@ -1,8 +1,10 @@
 import {Box} from "@mui/material";
+import {useTheme} from "@mui/material";
 
 function Body3() {
+    const theme = useTheme()
     return (
-        <Box sx={{bgcolor: "pink", height: "50vh", width: "100%"}}>
+        <Box sx={{bgcolor: {}, height: "50vh", width: "100%"}}>
             <p>Body 3</p>
         </Box>
     )

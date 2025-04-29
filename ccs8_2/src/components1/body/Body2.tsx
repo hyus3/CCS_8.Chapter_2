0f381db193258452bcf4ff30c19a3bcbbdf3699a
kerokenn <155714141+kerokenn@users.noreply.github.com>
2025-04-29 17:35:41 +0800

    lol
    lol

--- ccs8_2/src/components1/body/Body2.tsx
+++ ccs8_2/src/components1/body/Body2.tsx
@@ -2,7 +2,10 @@ import {Box} from "@mui/material";
 
 function Body2() {
     return (
-        <Box sx={{bgcolor: "lightgreen", height: "50vh", width: "100%"}}>
+        <Box sx={{
+            bgcolor: "Black", 
+            height: "50vh", 
+            width: "100%"}}>
             <p>Body 2</p>
         </Box>
     )
